cdd926ad57044c09b25d9f56d424a0d8b2a4094c
Akshat Dodhiya <dodhiyaakshat@gmail.com>
2020-05-08 15:34:17 +0530

    Reverted changes

--- quiz1.py
+++
@@ -1,7 +0,0 @@
-print("Enter your First Number")
-n1=input()
-
-print("Enter your Second Number")
-n2=input()
-
-print("Sum of the two Numbers is",int(n1) + int(n2))
--- tut17.py
+++
@@ -1,6 +0,0 @@
-i = 0 #Initializing a value to variable i
-
-while (i<45): #Using while loop with condition i<45
-    print(i) #Printing different values of i
-    i = i + 1 #Incrementing the value of i by 1 in each iterartion
-
